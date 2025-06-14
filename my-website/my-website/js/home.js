3e16a2527c20242cd17c2fbe5ca88ff083950e4d
yobab04 <oneyearwenny@gmail.com>
2025-06-15 07:23:34 +0800

    Update home.js

--- my-website/my-website/js/home.js
+++ my-website/my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'acd11652cfbb6ce367d1114252ec0792';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
